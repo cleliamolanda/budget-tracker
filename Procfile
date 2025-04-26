bfe9ef0925ce4a2b4d8ea3eaeecd71fe5d0163e7
Dallss <raalquicer@up.edu.ph>
2025-04-26 12:09:29 +0800

    Add health check endpoint and update Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && python manage.py collectstatic --noinput && gunicorn budget_tracker_backend.wsgi 
+web: python manage.py migrate && python manage.py collectstatic --noinput && gunicorn budget_tracker_backend.wsgi --bind 0.0.0.0:$PORT 
--- budget_tracker_backend/urls.py
+++ budget_tracker_backend/urls.py
@@ -18,10 +18,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('budget.urls')),
+    path('health/', health_check, name='health_check'),
 ]
