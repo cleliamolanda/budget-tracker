8857b1427be8407872d57744582f2781b15cb899
Dallss <raalquicer@up.edu.ph>
2025-04-27 15:32:48 +0800

    bug-fix: fixes logout button

--- budget_tracker_backend/urls.py
+++ budget_tracker_backend/urls.py
@@ -23,6 +23,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('budget.urls')),
 ]
